ac3d917d1e0f13cb2b71af5e6bd7dc236e8524bd
Liseon617 <64420980+Liseon617@users.noreply.github.com>
2023-06-09 15:55:54 +0800

    Completed mnist; error persisting in mnist_conv

--- network.py
+++ network.py
@@ -5,23 +5,20 @@ def predict(network, input):
     return output
 
 def train(network, loss, loss_prime, x_train, y_train, epochs = 1000, learning_rate = 0.01, verbose = True):
-    # train
     for e in range(epochs):
         error = 0
         for x, y in zip(x_train, y_train):
-            #forward
-            output = x
-            for layer in network:
-                output = layer.forward(output)
+            # forward
+            output = predict(network, x)
 
-            #error
+            # error
             error += loss(y, output)
 
-            #backward
-            grad = loss_prime(y , output)
+            # backward
+            grad = loss_prime(y, output)
             for layer in reversed(network):
                 grad = layer.backward(grad, learning_rate)
-    
-    error /= len(x_train)
-    if verbose:
-        print(f"{e + 1}/{epochs}, error={error}")
+
+        error /= len(x_train)
+        if verbose:
+            print(f"{e + 1}/{epochs}, error={error}")
